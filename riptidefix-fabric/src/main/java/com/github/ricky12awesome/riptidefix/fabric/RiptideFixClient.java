077ebe27a33b939e6c7273f49385a69b58c1bffa
CalXee <68349758+CalXee@users.noreply.github.com>
2021-12-18 18:47:28 -0500

    Update RiptideFixClient.java

--- riptidefix-fabric/src/main/java/com/github/ricky12awesome/riptidefix/fabric/RiptideFixClient.java
+++ riptidefix-fabric/src/main/java/com/github/ricky12awesome/riptidefix/fabric/RiptideFixClient.java
@@ -1,15 +1,10 @@
-package com.github.ricky12awesome.riptidefix.fabric;
+package com.github.ricky12awesome.riptidefix;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.loader.api.FabricLoader;
 
 public class RiptideFixClient implements ClientModInitializer {
-    public static boolean ENABLED = true;
-
     @Override
     public void onInitializeClient() {
-        if (FabricLoader.getInstance().isModLoaded("fabric")) {
-            com.github.ricky12awesome.riptidefix.fabric.fabricapi.RegisterToggleCommand.Register();
-        }
+
     }
 }
